b87db251c378d7e9b9ab5026fb6f55aac6afc509
vsevolod-viliatser <sevavilyatser@gmail.com>
2025-01-18 13:37:49 +0200

    logic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    CommonModule,  // Подключение CommonModule
+    FormsModule,   // Подключение FormsModule
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'contact/new', component: AppComponent }, // Новый контакт
+  { path: 'contact/edit/:id', component: AppComponent }, // Редактирование контакта
+  { path: 'contact/:id', component: AppComponent }, // Просмотр контакта
+];
